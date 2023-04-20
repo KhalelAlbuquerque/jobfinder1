48c61770bd4bf208b1e1546bc9a3f8cdafd1b156
khalelalbuquerque <khaleledipo@gmail.com>
2023-04-20 14:53:09 -0300

    banco de dados iniciado

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require("express")
 const app = express()
+const db = require('./db/connection')
 
 const port = 3000;
+
+
 
 app.listen(port, ()=>{
     console.log("Rodando o servidor :)")
 })
 
+
+//db connection
+db.authenticate().then(()=>{
+    console.log("Sucesso ao conectar ao DB")
+}).catch(err=>{
+    console.log("Ocorreu um erro ao conectar", err)
+})
+
+
+//routes
 app.get('/', (req,res)=>{
     res.send("Funcionando")
 })
