f5b23d750eabfe922a231a4ce348d14129fa22e5
khalelalbuquerque <khaleledipo@gmail.com>
2023-04-21 11:44:12 -0300

    funcao busca1

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app           = express()
 const path          = require('path')
 const db            = require('./db/connection')
 const bodyParser    = require("body-parser")
+const Job           = require('./models/Job')
+const Sequelize     = require('sequelize')
+const Op            = Sequelize.Op
 
 const port = 3000;
 
@@ -40,7 +43,27 @@ db.authenticate().then(()=>{
 
 //routes
 app.get('/', (req,res)=>{
-    res.render('index')
+
+    let search = req.query.job
+    let query = "%"+search+"%"
+
+    if(!search){
+
+        Job.findAll({order:[
+            ['createdAt', 'DESC']
+        ]}).then(jobs=>{
+            res.render('index', {jobs})
+        }).catch(err=>console.log(err))
+
+    }else{
+        Job.findAll({
+            where: {title:{[Op.like]: query}}, 
+            order:[
+            ['createdAt', 'DESC']
+        ]}).then(jobs=>{
+            res.render('index', {jobs, search})
+        }).catch(err=>console.log(err))
+    }
 })
 
 
